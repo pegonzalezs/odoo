ff3dd0adbc4e94469e1c5ad3f861dcc64a31adf5
Raul Martin Felez <raul.martin@brain-tec.ch>
2015-09-14 11:15:15 +0200

    New options to parse some other parameters send by console
    Conflicts:
    	openerp/tools/config.py

--- openerp/tools/config.py
+++ openerp/tools/config.py
@@ -305,7 +305,7 @@ class configmanager(object):
                          help="Use the unaccent function provided by the database when available.")
         group.add_option("--geoip-db", dest="geoip_database", my_default='/usr/share/GeoIP/GeoLiteCity.dat',
                          help="Absolute path to the GeoIP database file.")
-        
+
         group.add_option("--without_bt_swissdec", dest="without_bt_swissdec",
                           help="do not require bt_swissdec when installing sett_hr",
                           my_default=False)
@@ -315,7 +315,7 @@ class configmanager(object):
         group.add_option("--bt_fetch_mail_db_name", dest="bt_fetch_mail_db_name",
                           help="comma-separated list of database names in which we prevent sending/receiving emails",
                           my_default=False)  
-        
+
         parser.add_option_group(group)
 
         if os.name == 'posix':
