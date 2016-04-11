f7e7af0d50e8efcd622e2830f3c21a6a73ba7898
Raul Martin Felez <raul.martin@brain-tec.ch>
2016-04-11 18:12:28 +0200

    Migration of patch
    https://svn.brain-tec.ch/patches/70/70--general--parent_field_in_attrs_final.patch

--- addons/web/static/src/js/view_list.js
+++ addons/web/static/src/js/view_list.js
@@ -1079,6 +1079,7 @@ instance.web.ListView.List = instance.web.Class.extend( /** @lends instance.web.
     },
     render_cell: function (record, column) {
         var value;
+        parent_fields = null;
         if(column.type === 'reference') {
             value = record.get(column.id);
             var ref_match;
@@ -1143,10 +1144,13 @@ instance.web.ListView.List = instance.web.Class.extend( /** @lends instance.web.
                 record.set(column.id, false);
             }
         }
+        if (this.dataset.parent_view) {
+            parent_fields = this.dataset.parent_view.get_fields_values();
+        }
         return column.format(record.toForm().data, {
             model: this.dataset.model,
             id: record.get('id')
-        });
+        }, parent_fields);
     },
     render: function () {
         this.$current.empty().append(
@@ -2205,7 +2209,7 @@ instance.web.list.Column = instance.web.Class.extend({
             this.invisible = '1';
         } else { delete this.invisible; }
     },
-    modifiers_for: function (fields) {
+    modifiers_for: function (fields, parent_fields) {
         var out = {};
         var domain_computer = instance.web.form.compute_domain;
 
@@ -2214,7 +2218,7 @@ instance.web.list.Column = instance.web.Class.extend({
             var modifier = this.modifiers[attr];
             out[attr] = _.isBoolean(modifier)
                 ? modifier
-                : domain_computer(modifier, fields);
+                : domain_computer(modifier, fields, parent_fields);
         }
 
         return out;
@@ -2244,11 +2248,11 @@ instance.web.list.Column = instance.web.Class.extend({
      * @param {Number} [options.id] current record's id
      * @return {String}
      */
-    format: function (row_data, options) {
+    format: function (row_data, options, parent_fields) {
         options = options || {};
         var attrs = {};
         if (options.process_modifiers !== false) {
-            attrs = this.modifiers_for(row_data);
+            attrs = this.modifiers_for(row_data, parent_fields);
         }
         if (attrs.invisible) { return ''; }
 
