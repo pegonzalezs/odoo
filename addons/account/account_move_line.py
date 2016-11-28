9f581f2ec7ae0cf43da8ee1364a1ae81e46ccdca
mara1 <raul.martin@braintec-group.com>
2016-11-28 17:01:25 +0100

    Adding patch from BT-Accounting https://github.com/brain-tec/BT-Accounting/blob/8.0/bt_account/patches/bt_account_changes_account_move_line.patch

--- addons/account/account_move_line.py
+++ addons/account/account_move_line.py
@@ -1378,7 +1378,8 @@ class account_move_line(osv.osv):
             account_id = 'account_collected_id'
             base_sign = 'base_sign'
             tax_sign = 'tax_sign'
-            if journal.type in ('purchase_refund', 'sale_refund') or (journal.type in ('cash', 'bank') and total < 0):
+            #hack jool: if book_taxes_back is set and total > 0
+            if journal.type in ('purchase_refund', 'sale_refund') or (journal.type in ('cash', 'bank') and total < 0) or (journal.type in ('cash', 'bank') and vals.get('book_taxes_back', False) and total > 0):
                 base_code = 'ref_base_code_id'
                 tax_code = 'ref_tax_code_id'
                 account_id = 'account_paid_id'
