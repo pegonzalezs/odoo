0359133ce2294d0cf55665699d14f10f4bfd42cd
Alvaro <alvaro.estebanez@braintec-group.com>
2018-03-05 15:56:28 +0100

    [IMP] Updating start of week to monday

--- addons/web/static/lib/moment/moment.js
+++ addons/web/static/lib/moment/moment.js
@@ -954,7 +954,7 @@
         },
 
         _week : {
-            dow : 0, // Sunday is the first day of the week.
+            dow : 1, // Sunday is the first day of the week.
             doy : 6  // The week that contains Jan 1st is the first week of the year.
         },
 
