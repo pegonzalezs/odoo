6c87ab8e4f0d09a643fc6e2e1a84305ea7e9dada
Antoine Guenet (AGE) <age@odoo.com>
2018-06-12 15:06:08 +0200

    [ADD] base for citadel module

---
+++ local-addons/citadel/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'The Citadel',
+    'summary': 'Manage the training of future maesters.',
+    'description': """* Create and edit classes, with different levels.
+                      * Handle different sessions given by different maesters at different moments. 
+                      * Register attendees of those sessions.
+                      * Differentiate the sessions in preparation from the ones that will actually be given.
+                      * Archive sessions.""",
+    'author': 'Antoine Guenet',
+    'license': 'AGPL-3',
+    'website': 'http://www.odoo.com',
+    'category': 'Events',
+    'version': '11.0.1.0.0',
+    'depends': ['base']
+}
