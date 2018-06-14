8dd1cd66a0c8e679a95e22a237d3d5ab08a570fe
Antoine Guenet (AGE) <age@odoo.com>
2018-06-14 15:06:30 +0200

    [ADD] override copy() to automatically change names

--- local-addons/openacademy/models/course.py
+++ local-addons/openacademy/models/course.py
@@ -24,6 +24,21 @@ class Course(models.Model):
          "The name must be unique")
     ]
 
+    @api.multi
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))]
+        )
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+        
+        default['name'] = new_name
+        return super(Course, self).copy(default)
+
 #
 #     @api.depends('value')
 #     def _value_pc(self):
