331a6d7135cdff697063787dbd3f0f754503a1a0
Antoine Guenet (AGE) <age@odoo.com>
2018-06-14 08:05:27 +0200

    [ADD] separate files for models

--- local-addons/openacademy/models/session.py
+++ local-addons/openacademy/models/session.py
@@ -2,13 +2,6 @@
 
 from odoo import models, fields, api
 
-class Course(models.Model):
-    _name = 'openacademy.course'
-
-    name = fields.Char('Title',
-                       required=True)
-    description = fields.Text()
-
 class Session(models.Model):
     _name = 'openacademy.session'
 
@@ -19,7 +12,3 @@ class Session(models.Model):
                             digits=(6, 2),
                             help="Duration in days")
     seats = fields.Integer('Number of Seats')
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
