cb48d5f56b18cad618e475c5f3e151676eb09a58
Antoine Guenet (AGE) <age@odoo.com>
2018-06-19 08:53:18 +0200

    [ADD] session dashboard

--- local-addons/openacademy/__manifest__.py
+++ local-addons/openacademy/__manifest__.py
@@ -21,7 +21,10 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': [
+        'base',
+        'board'
+    ],
 
     # always loaded
     'data': [
@@ -30,6 +33,7 @@
         'views/templates.xml',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_board.xml',
         'reports.xml'
     ],
     # only loaded in demonstration mode
