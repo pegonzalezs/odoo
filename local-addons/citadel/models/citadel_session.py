efb81880e406510bbc83925fc20d244d54b26bd2
Antoine Guenet (AGE) <age@odoo.com>
2018-06-22 08:08:18 +0200

    [ADD] constraints on citadel session

--- local-addons/citadel/models/citadel_session.py
+++ local-addons/citadel/models/citadel_session.py
@@ -1,17 +1,32 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class CitadelSession(models.Model):
     _name = 'citadel.session'
-    _description = 'Citadel Session'
+    _description = "Citadel Session"
 
     active = fields.Boolean(default=True)
-    state = fields.Selection([('draft', 'In preparation'),
-                              ('ready', 'Ready')],
-                             'State')
-    time_event = fields.Datetime('Time of the session')
+    state = fields.Selection([('draft', "In preparation"),
+                              ('ready', "Ready")],
+                             "State")
+    time_event = fields.Datetime("Time of the session")
+    max_attendees = fields.Integer("Maximum number of Attendees")
+    completion_pc = fields.Float("Percentage of seats taken",
+                                   compute='_get_completion_pc',
+                                   readonly=True)
 
     course = fields.Many2one('citadel.course',
-                             string='Course',
+                             string="Course",
                              ondelete='cascade')
     attendees = fields.Many2many('res.partner',
-                                 string='Attendees')
+                                 string="Attendees")
+    
+    @api.constrains('max_attendees', 'attendees')
+    def _check_max_attendees(self):
+        if len(self.attendees) > self.max_attendees:
+            raise ValidationError("Too many attendees: either remove an attendee or raise the maximum number of attendees.")
+
+    @api.depends('max_attendees', 'attendees')
+    def _get_completion_pc(self):
+        for r in self:
+            r.completion_pc = 100 * (float(len(r.attendees))/r.max_attendees)
